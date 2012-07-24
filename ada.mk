8db281e4ff6ff23b2bdec59cc770d1258be5b187
Blueharford <blueharford@gmail.com>
2012-07-24 16:20:47 -0700

    add mk file
    Change-Id: I380c0f326c50c62b06289e8fe98163ca3cdf0edd

---
+++ ada.mk
@@ -0,0 +1,24 @@
+# Release name
+PRODUCT_RELEASE_NAME := GN-CDMA
+
+# Boot animation
+TARGET_BOOTANIMATION_NAME := vertical-720x1280
+
+# Inherit some common ADA stuff.
+$(call inherit-product, vendor/ada/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/toro/full_toro.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/ada/config/nfc_enhanced.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := toro
+PRODUCT_NAME := ada_toro
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Galaxy Nexus
+PRODUCT_MANUFACTURER := Samsung
+
+#Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mysid BUILD_FINGERPRINT="google/mysid/toro:4.0.4/IMM76K/336647:user/release-keys" PRIVATE_BUILD_DESC="mysid-user 4.0.4 IMM76K 336647 release-keys"
